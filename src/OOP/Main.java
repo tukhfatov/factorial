052490ecb1479274c59c7192590f2cbbeddb1528
Margulan Tukhfatov <tukhfatov.margulan@gmail.com>
2015-05-20 22:56:00 +0800

    First Commit

---
+++ src/OOP/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP;
+
+/**
+ *
+ * @author tukhfatov
+ */
+public class Main {
+    public static void main(String args[]){
+        
+    }
+}
